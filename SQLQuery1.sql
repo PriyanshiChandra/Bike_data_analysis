fecf7bf8083c7c7ac9d15c2e5b917cc117dc5a72
Priyanshi Chandra <66741531+PriyanshiChandra@users.noreply.github.com>
2024-08-18 15:25:35 +0530

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -11,7 +11,7 @@ select * from bike_share_yr_1)
 select 
 dteday, season, a.yr, weekday, hr, rider_type, 
 riders, price,COGS, riders*price as revenue,
-riders*price - COGS as profit
+riders*price - COGS*riders as profit
 from cte a
 left join cost_table b
 on a.yr = b.yr
